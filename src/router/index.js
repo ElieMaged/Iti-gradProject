dfb791df6ab693d223bd33b56678d52fa73009d1
ElieMaged <Narutossj23@yahoo.com>
2025-07-19 15:49:11 +0300

    q

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,13 @@ import { createWebHistory, createRouter } from 'vue-router'
 import HomePage from '../views/HomePage.vue'
 import About from '../views/About.vue'
 import Contact from '../views/Contact.vue'
+import UserSignUp from '../views/UserSignUp.vue'
 
 const routes = [
   { path: '/', component: HomePage },
   { path: '/about', component: About },
   { path: '/contact', component: Contact },
+  { path: '/usersignup', component: UserSignUp },
 ]
 
 const router = createRouter({
