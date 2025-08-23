4ba4ccf5b8add8898f657f4ee173ac26c47c76c0
tasneem Mostafa <tasneemmostafa200110@gmail.com>
2025-08-23 20:10:16 +0300

    trying fix

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,32 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    vue()
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => tag === 'authState'
+        }
+      }
+    })
   ],
   base: '/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: false,
+    sourcemap: true,
     minify: 'terser',
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         manualChunks: {
@@ -23,19 +36,21 @@ export default defineConfig({
         },
         chunkFileNames: 'assets/js/[name]-[hash].js',
         entryFileNames: 'assets/js/[name]-[hash].js',
-        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]'
+        assetFileNames: 'assets/[ext]/[name]-[hash][extname]'
       }
     },
     terserOptions: {
       compress: {
         drop_console: true,
-        drop_debugger: true
+        drop_debugger: true,
+        pure_funcs: ['console.log']
       }
     }
   },
   server: {
     port: 3000,
-    host: true
+    host: true,
+    strictPort: true
   },
   preview: {
     port: 3000,
